9c0f120ec892626950952517716a7ce955be4c8f
zoghlamimeda <g.meda69@gmail.com>
2020-05-06 21:46:27 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/HuffmanCoding.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HuffmanCoding.dir/main.c.obj"
+  "HuffmanCoding.exe"
+  "HuffmanCoding.exe.manifest"
+  "HuffmanCoding.pdb"
+  "libHuffmanCoding.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HuffmanCoding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
